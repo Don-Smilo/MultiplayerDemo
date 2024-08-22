a2d65eca380ef46e26278e1943ccc08cfad6ad15
Don-Smilo <don_smilo@mail.ru>
2024-08-22 15:23:21 +0300

    Add pause menu

--- Source/MultiplayerDemo/InGamePlayerController.cpp
+++ Source/MultiplayerDemo/InGamePlayerController.cpp
@@ -2,4 +2,52 @@
 
 
 #include "InGamePlayerController.h"
+#include "InGameHUD.h"
+#include "Kismet/GameplayStatics.h"
+#include <EnhancedInputSubsystems.h>
 
+void AInGamePlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem< UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+
+	if (Subsystem)
+	{
+		Subsystem->AddMappingContext(DefaultMappingContext, 0);
+	}
+}
+
+void AInGamePlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent))
+	{
+		EnhancedInputComponent->BindAction(PauseAction, ETriggerEvent::Started, this, &AInGamePlayerController::TogglePause);
+	}
+
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("Input Component NOT FOUND"));
+	}
+}
+
+void AInGamePlayerController::TogglePause(const FInputActionValue& Value)
+{
+	AInGameHUD* InGameHud = Cast<AInGameHUD>(GetHUD());
+	if (InGameHud)
+	{
+		if (!bIsPaused)
+		{
+			InGameHud->PauseMenuShow();
+		}
+
+		else
+		{
+			InGameHud->PauseMenuHide();
+		}
+
+		bIsPaused = !bIsPaused;
+	}
+}
--- Source/MultiplayerDemo/InGamePlayerController.h
+++ Source/MultiplayerDemo/InGamePlayerController.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "EnhancedInputComponent.h"
+#include "InputActionValue.h"
 #include "InGamePlayerController.generated.h"
 
 /**
@@ -13,5 +15,21 @@ UCLASS()
 class MULTIPLAYERDEMO_API AInGamePlayerController : public APlayerController
 {
 	GENERATED_BODY()
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+
+
+private:
+	void TogglePause(const FInputActionValue& Value);
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input", meta = (AllowPrivateAccess = "true"))
+	UInputAction* PauseAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input", meta = (AllowPrivateAccess = "true"))
+	UInputMappingContext* DefaultMappingContext;
+
+	bool bIsPaused = false;
 	
 };
